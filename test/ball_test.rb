dcc5477040419eb8f5d7a3ee0d92c97eddee83e5
George Ho <george.h.w.ho@gmail.com>
2021-02-08 18:38:56 -0500

    Add ball and ball tests

--- test/ball_test.rb
+++ test/ball_test.rb
@@ -2,4 +2,13 @@ require_relative 'test_helper'
 require './lib/ball'
 
 class BallTest < Minitest::Test
-end 
+  def test_it_exists
+    ball = Ball.new('r')
+    assert_instance_of Ball, ball
+  end
+
+  def test_has_attributes
+    ball = Ball.new('r')
+    assert_equal 'r', ball.color
+  end
+end
