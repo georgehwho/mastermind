aedffda4b3339a28061cafc597c73933adfad8cf
George Ho <george.h.w.ho@gmail.com>
2021-02-08 16:32:36 -0500

    Add pin instance of test

--- test/pin_test.rb
+++ test/pin_test.rb
@@ -2,4 +2,8 @@ require_relative 'test_helper'
 require './lib/pin'
 
 class PinTest < Minitest::Test
+  def test_it_exits
+    pin = Pin.new(:correct_position)
+    assert_instance_of Pin, pin
+  end
 end
