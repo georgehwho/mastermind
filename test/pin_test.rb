f8abde87253962cff99a498e2aa44dfae26f84fa
George Ho <george.h.w.ho@gmail.com>
2021-02-08 18:27:34 -0500

    Fix typo in one of the tests

--- test/pin_test.rb
+++ test/pin_test.rb
@@ -2,7 +2,7 @@ require_relative 'test_helper'
 require './lib/pin'
 
 class PinTest < Minitest::Test
-  def test_it_exits
+  def test_it_exists
     pin = Pin.new()
     assert_instance_of Pin, pin
   end
