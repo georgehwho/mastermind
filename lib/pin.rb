2e5e08ff7c9ff206b40e7677ccb2aa621ec9874f
George Ho <george.h.w.ho@gmail.com>
2021-02-08 18:26:12 -0500

    Add pin and pin tests

--- lib/pin.rb
+++ lib/pin.rb
@@ -1,2 +1,17 @@
 class Pin
-end 
+  attr_reader :correct, :position
+
+  def initialize()
+    @correct = false
+    @position = false
+  end
+
+  def only_correct
+    @correct = true
+  end
+
+  def both_correct
+    @correct = true
+    @position = true
+  end
+end
--- test/pin_test.rb
+++ test/pin_test.rb
@@ -3,7 +3,27 @@ require './lib/pin'
 
 class PinTest < Minitest::Test
   def test_it_exits
-    pin = Pin.new(:correct_position)
+    pin = Pin.new()
     assert_instance_of Pin, pin
+  end
+
+  def test_has_accessible_attributes
+    pin = Pin.new()
+    assert_equal false, pin.position
+    assert_equal false, pin.correct
+  end
+
+  def test_just_correct_works
+    pin = Pin.new()
+    pin.only_correct
+    assert_equal false, pin.position
+    assert_equal true, pin.correct
+  end
+
+  def test_both_correct_works
+    pin = Pin.new()
+    pin.both_correct
+    assert_equal true, pin.position
+    assert_equal true, pin.correct
   end
 end
