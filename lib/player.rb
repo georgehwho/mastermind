8f0f531e41ac43570f861056dd681e9b9025a6a6
George Ho <george.h.w.ho@gmail.com>
2021-02-08 19:00:02 -0500

    Add player and player tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,20 @@
 class Player
-end 
+  attr_reader :balls, :pins
+
+  def initialize()
+    @balls = []
+    @pins = []
+  end
+
+  def generate(num_balls)
+    ball_storage = []
+    num_balls.times do
+      ball_storage << 'r'
+      ball_storage << 'g'
+      ball_storage << 'y'
+      ball_storage << 'b'
+    end
+    random_balls = ball_storage.shuffle[0..num_balls - 1]
+    random_balls.each { |ball| @balls << Ball.new(ball) }
+  end
+end
