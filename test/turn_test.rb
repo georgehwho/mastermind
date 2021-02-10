eefbc813d46359573203e3b68cd2ace212d0f337
Joe Ray <jray100.jr@gmail.com>
2021-02-09 21:56:41 -0700

    Added comments and started Turn files

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -5,4 +5,29 @@ require './lib/pin'
 require './lib/ball'
 
 class TurnTest < Minitest::Test
+
+  def setup
+    @turn = Turn.new(@player, @round)
+    @player = Player.new(@balls, @pins)
+  end
+
+  def test_turn_exist
+    assert_instance_of Turn, @turn
+  end
+
+  def test_
+    assert_equal
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
