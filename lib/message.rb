b1f476ae17fb56a653642ee6b958a01c528c7202
Joe Ray <jray100.jr@gmail.com>
2021-02-12 19:07:39 -0700

    Format messages for clarity and utility

--- lib/message.rb
+++ lib/message.rb
@@ -1,24 +1,35 @@
 module Message
 
-  def welcome
-    'Welcome to MASTERMIND'
+  def turn_result(guess = '', results = [0,0], round = 0)
+    "#{guess.upcase} has #{results[0]} of the correct elements with #{results[1]} in the correct positions \n You've taken #{round} guesses"
   end
 
-  def game_start
-    'Would you like to (p)lay, read the (i)instructions, or (q)uit?'
+  def tier_1
+    "Look at you go"
   end
 
-  def turn_prompt
-    "I have generated a beginner sequence with four elements made up of: (r)ed,
-    (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game.
-    What's your guess?"
+  def tier_2
+    "You heating up but you won't do it again."
   end
 
-  def turn_result
-    "#{turn.player.list_balls} has #{turn.place_pins[0]} of the correct elements with #{turn.place_pins[1]} in the correct positions
-    You've taken #{number of guesses} guesses"
+  def tier_3
+    "I let you have that one!"
   end
 
+  def tier_4
+    "Whatever...you win I guess. Best two out of three?!"
+  end
+
+  def user_error_msgs
+    {
+      greater4: 'too many balls!!!',
+      less4: 'too little balls!!!',
+      general: 'you messed up in general.',
+      bad_inputs: "you didn't input the correct colors",
+      bad_instructions: "you didn't enter a valid instruction"
+    }
+  end
+  # Turn Prompts above ______________________________________Game prompts below
   def end_game
     "Congratulations! You guessed the sequence #{player answer } in #{guesses} guesses over #{time}"
     # retry method here
@@ -34,30 +45,14 @@ module Message
   end
 
   def quitting
-    #goodbye msg and game exits
+    'goodbye msg and game exits'
   end
 
-  def tier_1
-    "Look at you go"
-  end
-
-  def tier_2
-    "You heating up but you won't do it again."
-  end
-
-  def tier_3
-    "I let you have that one!"
-  end
-
-  def method_name
-    "Whatever...you win I guess. Best two out of three?!"
-  end
-
-  def user_error_msgs
+  def game_msgs
     {
-      greater4: 'too many balls!!!',
-      less4: 'too little balls!!!',
-      general: 'you messed up in general.'
+      game_start: 'Would you like to (p)lay, read the (i)instructions, or (q)uit?',
+      welcome: 'Welcome to MASTERMIND!',
+      turn_prompt: "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game. What's your guess?",
     }
   end
 
