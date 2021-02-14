0820b9afdd47e7c03101d8a50fb500555f8ea565
George Ho <george.h.w.ho@gmail.com>
2021-02-13 22:53:31 -0500

    Refactor player class with tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,7 @@
 class Player
   attr_reader :balls,
-              :pins
+              :pins,
+              :ball_storage
 
   def initialize()
     @balls = []
@@ -10,12 +11,26 @@ class Player
   def generate(num_balls = 4)
     @balls = []
     @pins = []
-    ball_storage = []
-    num_balls.times do
-      ball_storage << 'r' << 'g' << 'y' << 'b'
-    end
+    generate_easy if num_balls == 4
+    generate_intermediate if num_balls == 6
+    generate_hard if num_balls == 8
     random_balls = ball_storage.shuffle[0..num_balls - 1]
     random_balls.each { |ball| @balls << Ball.new(ball) }
+  end
+
+  def generate_easy
+    @ball_storage = []
+    4.times { @ball_storage << 'r' << 'g' << 'y' << 'b' }
+  end
+
+  def generate_intermediate
+    @ball_storage = []
+    6.times { @ball_storage << 'r' << 'g' << 'y' << 'b' << 'o' }
+  end
+
+  def generate_hard
+    @ball_storage = []
+    8.times { @ball_storage << 'r' << 'g' << 'y' << 'b' << 'o' << 'm' }
   end
 
   def set_answer(player_response)
@@ -25,24 +40,27 @@ class Player
   end
 
   def check_answer(answer)
-    counter = 0
-    ball_holder = list_balls
     ball_array = answer.chars
+    check_correct_pins(ball_array)
+    check_pin_position(ball_array)
+  end
 
-    ball_array.each_with_index do |ball, index|
+  ####
+  def check_correct_pins(balls)
+    ball_holder = list_balls
+    balls.each_with_index do |ball, index|
       if ball_holder.include?(ball)
-        counter += 1
+        @pins << Pin.new()
+        @pins.find { |pin| pin.correct == false }.only_correct
         ball_holder.delete_at(ball_holder.index(ball))
       end
     end
+  end
 
-    counter.times do
-      @pins << Pin.new()
-      @pins.find { |pin| pin.correct == false }.only_correct
-    end
-
+  ####
+  def check_pin_position(balls)
     list_balls.each_with_index do |ball, index|
-      if ball_array[index] == ball
+      if balls[index] == ball
         @pins.find { |pin| pin.position == false }.both_correct
       end
     end
