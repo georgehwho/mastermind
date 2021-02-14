3824f8bb25e561503fe657c0633e2653ef5e21cd
George Ho <george.h.w.ho@gmail.com>
2021-02-14 15:01:10 -0500

    Fix pick difficulty to display proper turn prompt

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,11 @@ require 'benchmark'
 
 class Game
   include Message
-  attr_reader :turn, :round, :time1, :time2
+  attr_reader :turn,
+              :round,
+              :time1,
+              :time2,
+              :difficulty
 
   def initialize
     @turn = Turn.new
@@ -33,7 +37,6 @@ class Game
 
   def start_prompt
     pick_difficulty # this is a method
-    puts game_msgs[:turn_prompt]
     @round = 0
     @time1 = Time.now
     playing # this is a method
@@ -44,11 +47,17 @@ class Game
     print '> '
     input = sanitized_gets
     if input == 'b'
+      @difficulty = 4
       turn.start_phase(4)
+      puts game_msgs[:beginner_turn_prompt]
     elsif input == 'i'
+      @difficulty = 6
       turn.start_phase(6)
+      puts game_msgs[:intermediate_turn_prompt]
     elsif input == 'a'
+      @difficulty = 8
       turn.start_phase(8)
+      puts game_msgs[:advanced_turn_prompt]
     else
       puts game_msgs[:bad_instructions]
       puts
@@ -77,9 +86,9 @@ class Game
       puts quitting
     else
       @round += 1
-      puts turn.guess(input, round)
+      puts turn.guess(input, round, difficulty)
 
-      if turn.pin_results == [4,4]
+      if turn.win?(difficulty)
         @time2 = Time.now
         puts
         puts end_game(input, round, time1, time2)
